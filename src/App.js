6e4f8c77b3905aca13860b5eabbce44fbd2724d3
Ayodeji adeyeye <adeyeye17@gmail.com>
2021-08-09 01:20:42 +0200

    ADDED: project.js file in components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      My App
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      projects:[
+        {
+          title: 'business Website',
+          category: 'Mobile Design' 
+          },
+        {
+          title: 'Social App',
+          category: 'Mobile Development' 
+          },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Mobile Development' 
+          },
+      ]
+    }
+  }
+  render() {
+    return(
+      <div className = "App">
+        My App
+        <Projects projects= {this.state.projects} />
+        
+    
+      </div>
+    );  
+  }
 }
 
 export default App;
+
---
+++ src/Components/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+
+  render() {
+   
+    console.log(this.props)
+    return(
+      <div className = "Projects">
+      
+      </div>
+    );
+  }
+}
+
+export default Projects;
+
